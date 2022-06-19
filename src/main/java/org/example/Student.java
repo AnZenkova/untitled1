7b070d915b61e3f7d6a9b6c0877e19166737772d
AnZenkova <nastyonka16@mail.ru>
2022-06-19 22:42:07 +1000

    Student

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Student {
+
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Student student = (Student) other;
+        return age == student.age && Objects.equals(name, student.name);
+    }
+}
